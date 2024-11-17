66903888d462a5786bc29e387b5813015a8c5d15
Novi-Simanjuntak <novisimanjuntak853@gmail.com>
2024-11-17 22:44:58 +0700

    perubahan materi dasar

--- src/pages/MateriDasar.js
+++ src/pages/MateriDasar.js
@@ -1,6 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const MateriDasar = () => {
+  const [selectedMateri, setSelectedMateri] = useState(null);
+
+  const handleMateriClick = (index) => {
+    selectedMateri(selectedMateri === index ? null : index );
+  };
   return (
     <div>
       <h2>Materi</h2>
