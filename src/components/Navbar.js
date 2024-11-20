447e8802a3bc6db5fe111083d297ccf6b4a21bfd
hugosmrgkr <hugosimorangkir10@gmail.com>
2024-11-20 10:29:07 +0700

    update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -79,7 +79,7 @@ const Navbar = () => {
             <ul className="navbar-nav ms-auto">
               <li className="nav-item">
                 <Link className="nav-link" to="/materi-dasar">
-                  Materi Dasar
+                  Materi
                 </Link>
               </li>
               <li className="nav-item">
